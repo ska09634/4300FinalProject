80c70a91915b474643c0fc18a7323f67eada9307
Chelsea Villard <cav90839@uga.edu>
2022-11-14 20:37:17 -0500

    Fixed the link issue, sending the add method over

--- final-project/src/components/Header.js
+++ final-project/src/components/Header.js
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-        <h1 className="banner" id="header">My Zoo</h1>
-        <button className="add-btn">Login</button>
-        <button className="edit-btn">Logout</button>
-        <button className="delete-btn">Signup</button>
-        <div className="list">
+    <h1 className="banner" id="header">My Zoo</h1>
+      <Link to='/login'>
+      <button className="logout-btn">Logout</button>
+      <div className="list">
           <h2>My Animals</h2>
          <ul id="dynamic-list"></ul>
       </div>
+      </Link>
     </header>
     
   )
