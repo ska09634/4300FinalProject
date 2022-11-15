5f66f7a4b9fa54abefb5eabc66432a056893a80d
Chelsea Villard <cav90839@uga.edu>
2022-11-14 20:20:26 -0500

    Adjusted page-layout with list

--- final-project/src/components/Header.js
+++ final-project/src/components/Header.js
@@ -5,7 +5,12 @@ const Header = () => {
         <button className="add-btn">Login</button>
         <button className="edit-btn">Logout</button>
         <button className="delete-btn">Signup</button>
+        <div className="list">
+          <h2>My Animals</h2>
+         <ul id="dynamic-list"></ul>
+      </div>
     </header>
+    
   )
 }
 
